2e65d979feae5488a8eb9e3eb3a408944834c719
Marina <mfuster@itba.edu.ar>
2019-06-28 06:27:36 -0300

    Add little functionality

--- parser.c
+++ parser.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "hashmap.h"
+#include "parser.h"
 
 map_t map=NULL;
 
@@ -9,4 +10,18 @@ void init_parser()
     map=hashmap_new();
 }
 
+int is_declared(char * key)
+{
+    any_t pointer=malloc(sizeof(*pointer));
+    int result=hashmap_get(map,key,pointer);
+    free(pointer);
+    if(result==MAP_OK)
+        return DECLARED;
+    return UNDECLARED;
+}
+
+// Stores pair key-value. Returns undeclared if there was any problem
+int update_sym_table(char * key, any_t value){
+    return hashmap_put(map, key, value)==MAP_OK? DECLARED : UNDECLARED;
+}
 
--- parser.h
+++ parser.h
@@ -6,6 +6,7 @@
 #define UNDECLARED 0
 #define DECLARED 1
 
+typedef void * any_t;
 typedef enum { TYPE_UNDEF, TYPE_NUMBER, TYPE_STRING, TYPE_REG } var_type_t;
 
 //typedef enum { OP_GREATER, OP_GREATER_EQ, OP_LESSER, OP_LESSER_EQ, OP_EQUALS, OP_NOT_EQ } op_t;
@@ -22,6 +23,9 @@ struct sym
 
 //void warning(char *s, char *t);
 void init_parser();
+int is_declared(char * key);
+int update_sym_table(char * key, any_t value);
+
 int numOfDigits(int n);
 int yywrap();
 
