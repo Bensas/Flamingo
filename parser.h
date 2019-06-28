56b3c61aef2dc9e97f097b0e427e16f7a3057656
Marina <mfuster@itba.edu.ar>
2019-06-28 05:55:02 -0300

    Comment on things that we do not need

--- parser.h
+++ parser.h
@@ -1,27 +1,27 @@
 #ifndef __PARSER_H
 #define __PARSER_H
 
-#define NSYMS 20 /* maximum number of symbols */
+//#define NSYMS 20 /* maximum number of symbols */
 
 #define UNDECLARED 0
 #define DECLARED 1
 
-typedef enum { TYPE_UNDEF, TYPE_NUMBER, TYPE_STRING } var_type_t;
+typedef enum { TYPE_UNDEF, TYPE_NUMBER, TYPE_STRING, TYPE_REG } var_type_t;
 
-typedef enum { OP_GREATER, OP_GREATER_EQ, OP_LESSER, OP_LESSER_EQ, OP_EQUALS, OP_NOT_EQ } op_t;
+//typedef enum { OP_GREATER, OP_GREATER_EQ, OP_LESSER, OP_LESSER_EQ, OP_EQUALS, OP_NOT_EQ } op_t;
 
-struct symtab
+struct sym
 {
     char *name;
     char *str;
     var_type_t var_type;
     int isDeclared;
-} symtab[NSYMS];
+} sym;
 
-struct symtab *symlook();
+//struct symtab *symlook();
 
-void warning(char *s, char *t);
+//void warning(char *s, char *t);
 int yywrap();
-
 int numOfDigits(int n);
+
 #endif
